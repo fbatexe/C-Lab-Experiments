bf17b445af28a2d039e37b9016e4601b02515c61
Arpan Pandey <pandeyarpan2006@gmail.com>
2025-12-21 16:26:33 +0530

    Experiment 2

--- Experiment-02/CW2.c
+++ Experiment-02/CW2.c
@@ -9,8 +9,9 @@ int main() {
     printf("Enter temperatue in celsius :");
     scanf("%f", &c);                            // Taking input temperature as celcius.       
 
-    f = (c*(9.0/5.0) + 32);                     // Formula to convert celsius to Fahrenheit
+    f = (c * (9.0/5.0) + 32);                     // Formula to convert celsius to Fahrenheit
     printf("Your temperature in Fahrenheit : %.2f",f);
     
     return 0;
+
 }
